b5b3b488dd268a0f0e0a339d5be22a1ca5b9352b
xiaoweicheng <2193485092@qq.com>
2020-01-07 08:29:14 +0800

    删除打印代码

--- socketIO/socketIO_server.js
+++ socketIO/socketIO_server.js
@@ -3,7 +3,6 @@ module.exports = function (server) {
   const io = require('socket.io')(server);
   io.on('connection', (socket) => {
     socket.on('sendMsg', ({from,to,content}) => {
-      console.log('服务器接收到的消息',{from,to,content});
       //保存消息
       const chat_id=[from,to].sort().join('_');
       const create_time=Date.now();
